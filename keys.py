055ff0e78d33dacaf59f34830925319761f62ced
aftabgit786 <gb485585@gmail.com>
2023-05-01 12:57:23 +0500

    update in code

--- keys.py
+++ keys.py
@@ -1,5 +1,8 @@
 import os
 
 
-account_sid = os.environ['TWILIO_ACCOUNT_SID'] = 'AC59bee873d1736f14f15e770eb398b14c'
-auth_token = os.environ['TWILIO_AUTH_TOKEN'] = 'a2d53968efeb60d5e11903f19b254406'
+os.environ['TWILIO_ACCOUNT_SID'] = 'AC59bee873d1736f14f15e770eb398b14c'
+os.environ['TWILIO_AUTH_TOKEN'] = 'a2d53968efeb60d5e11903f19b254406'
+
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
