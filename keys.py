5fc43a917f4aa8d006a57f05719e70d4ce8d0ad7
aftabgit786 <gb485585@gmail.com>
2023-05-01 11:53:58 +0500

    delete 2 lines

--- keys.py
+++ keys.py
@@ -1,8 +1,5 @@
 import os
 
 
-os.environ['TWILIO_ACCOUNT_SID'] = 'AC59bee873d1736f14f15e770eb398b14c'
-os.environ['TWILIO_AUTH_TOKEN'] = 'a2d53968efeb60d5e11903f19b254406'
-
-account_sid = os.environ['TWILIO_ACCOUNT_SID']
-auth_token = os.environ['TWILIO_AUTH_TOKEN']
+account_sid = os.environ['TWILIO_ACCOUNT_SID'] = 'AC59bee873d1736f14f15e770eb398b14c'
+auth_token = os.environ['TWILIO_AUTH_TOKEN'] = 'a2d53968efeb60d5e11903f19b254406'
